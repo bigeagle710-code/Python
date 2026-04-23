bfc8938541af18e6065e46bbdb24b8a50a8d4717
bigeagle710-code <bigeagle710@gmail.com>
2026-04-23 23:37:45 +0500

    Add files via upload

---
+++ Summer times.py
@@ -0,0 +1,6 @@
+print ("Summer holidays conditions")
+m=input("Have you done your homework Y or N: ")
+if m == 'Y':
+    print("You can play as much you want and enjoy your holidays")
+else:
+    print("You should complete your homework first!!")
