60cb290ddbd734eccb555a12c38519a6c652bc03
bigeagle710-code <bigeagle710@gmail.com>
2026-05-15 16:09:38 +0500

    Add files via upload
    fahh

--- mirrored triangle.py
+++ mirrored triangle.py
@@ -1,4 +1,9 @@
 rows = 5
+
+print("Normal Triangle:")
 for i in range(1, rows + 1):
-    
-    print(" " * (rows - i) + "*" * i)
+    print('*' * i)
+
+print("\nMirrored Triangle:")
+for i in range(1, rows + 1):
+    print(' ' * (rows - i) + '*' * i)
