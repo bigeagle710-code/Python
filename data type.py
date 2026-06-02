e833aa0eff7fe291420005ae17954f700a660d68
bigeagle710-code <bigeagle710@gmail.com>
2026-06-02 19:43:21 +0500

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,11 @@
+a=10
+print("A is:",a,"\nData type of A is:",type(a))
+b=5.5
+print("B is:",b,"\nData type of B is:",type(b))
+c=1+2j
+print("C is:",c,"\nData type of C is:",type(c))
+d="Hello"
+print("D is:",d,"\nData type of D is:",type(d))
+e=True
+e1=False
+print("E is:",e,"\nData type of E is:",type(e))
