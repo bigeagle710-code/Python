488383064f6bdbd9766973367fddb658ace29e70
bigeagle710-code <bigeagle710@gmail.com>
2026-06-09 20:45:10 +0500

    Add files via upload

---
+++ tuple operation.py
@@ -0,0 +1,13 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+tuplex = (4, 6, 2, 8, 3, 1)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+tuple1 = (67,69,67,71,67,73,67)
+print(tuple1.count(50))
+tuplex = (2, 4, 6, 8, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+_slice = tuplex[3:5]
+print(_slice)
+_slice = tuplex[:6]
+print(_slice)
